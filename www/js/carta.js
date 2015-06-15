5e3dc41ae190502380267359f492f7101d9d86f2
takeuchi <g.takeuchi@gmail.com>
2015-06-14 22:17:47 -0300

    som de carta virando e correcao da imagem do bombeiro

--- www/js/carta.js
+++ www/js/carta.js
@@ -14,6 +14,27 @@ function Carta(tab, morador) {
         self.imagemAtual(self.morador.imagemCasa())
     }
 
+    self.marcaCapturado = function(cacador) {
+        var pr = $("#"+self.id)
+        pr.disableTransitions();
+        pr.css("background-image", cacador.imagemMorador());
+        pr.append($("<img class='gotcha' src='../imgs/ok.png'>"));
+        pr.css("opacity", "0.5");
+    }
+
+    self.marcaAberta = function() {
+        app.defer(function() {
+            $("#"+self.id).css("opacity", "0.5");
+        }, 1000)
+
+    }
+
+    self.oculta = function() {
+        app.defer(function () {
+            self.visivel(false);
+        }, 1000)
+    }
+
     this.revela = function() {
         tab.abriuBloco(self);
     }
